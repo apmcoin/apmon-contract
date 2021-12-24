92f8a648c9e33beec0b1d7b5ac64675cec1d206a
jeon <46668752+apmcoin-dev@users.noreply.github.com>
2021-12-24 17:46:44 +0900

    Verify after deploy to testnet

--- scripts/deploy.ts
+++ scripts/deploy.ts
@@ -1,13 +1,31 @@
 import { ethers } from "hardhat";
 
 async function main() {
-
+  const [deployer] = await ethers.getSigners();
   const ApMON = await ethers.getContractFactory("ApMON");
   const apMON = await ApMON.deploy();
-
   await apMON.deployed();
 
+  var tokenIds: Array<Number> = new Array<Number>();
+  var uris: Array<String> = new Array<String>();
+
+  for (let i = 1; i <= 5; i++) {
+    let idx = i - 1;
+    tokenIds[idx] = i;
+    uris[idx] = "https://apmon.apm-coin.com/matadata/test-" + i + ".json";
+  }
+
+  //test mint
+  const batchMintTx = await apMON.safeBatchMint(deployer.address, tokenIds, uris);
+  await batchMintTx.wait();
+  //npx hardhat run --network bsctestnet scripts/deploy.ts
+
+  const ownerBalance = await apMON.balanceOf(deployer.address);
+  console.log("apMON totalSupply:", ownerBalance);
   console.log("apMON deployed to:", apMON.address);
+
+  //verify exam
+  //npx hardhat verify --network bsctestnet DEPLOYED_CONTRACT_ADDRESS
 }
 
 main().catch((error) => {
