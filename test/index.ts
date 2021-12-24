abc3b2e587bc04f80b9ff8a1c9423b2e14cef0ae
jeon <46668752+apmcoin-dev@users.noreply.github.com>
2021-12-24 17:30:32 +0900

    Batchmint test

--- test/index.ts
+++ test/index.ts
@@ -1,19 +1,33 @@
 import { expect } from "chai";
 import { ethers } from "hardhat";
 
-describe("Greeter", function () {
-  it("Should return the new greeting once it's changed", async function () {
-    const Greeter = await ethers.getContractFactory("Greeter");
-    const greeter = await Greeter.deploy("Hello, world!");
-    await greeter.deployed();
+interface nft{
+  tokenId: Number;
+  uri: String;
+}
 
-    expect(await greeter.greet()).to.equal("Hello, world!");
+describe("ApMON", function () {
+  it("deploy and initial mint", async function () {
+    const [owner] = await ethers.getSigners();
+    const ApMON = await ethers.getContractFactory("ApMON");
+    const apMON = await ApMON.deploy();
+    await apMON.deployed();
 
-    const setGreetingTx = await greeter.setGreeting("Hola, mundo!");
+    var tokenIds: Array<Number> = new Array<Number>();
+    var uris: Array<String> = new Array<String>();
 
-    // wait until the transaction is mined
-    await setGreetingTx.wait();
+    for (let i = 1; i <= 5; i++) {
+      let idx = i - 1;
+      tokenIds[idx] = i;
+      uris[idx] = "https://apmon.apm-coin.com/matadata/test-" + i + ".json";
+    }
 
-    expect(await greeter.greet()).to.equal("Hola, mundo!");
+    expect(await apMON.totalSupply()).to.equal(0);
+
+    const batchMintTx = await apMON.safeBatchMint(owner.address, tokenIds, uris);
+    await batchMintTx.wait();
+
+    const ownerBalance = await apMON.balanceOf(owner.address);
+    expect(await apMON.totalSupply()).to.equal(ownerBalance);
   });
 });
